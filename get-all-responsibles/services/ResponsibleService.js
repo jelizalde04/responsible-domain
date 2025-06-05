a3782199604b5ce210579275a81b22c32b3f9704
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-04 19:55:52 -0500

    feat (service): 🔐 add logic to fetch all users

---
+++ get-all-responsibles/services/ResponsibleService.js
@@ -0,0 +1,10 @@
+const Responsible = require("../models/Responsible");
+
+const getAllResponsibles = async () => {
+  // Usamos findAll para obtener todos los responsables de la base de datos
+  const responsibles = await Responsible.findAll();  // Obtenemos todos los responsables
+
+  return responsibles;
+};
+
+module.exports = { getAllResponsibles };  // Asegúrate de exportar correctamente
