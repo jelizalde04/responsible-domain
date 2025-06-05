09ab03f25cefd25f19d6674723a6689269984cf1
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-04 19:52:31 -0500

    docs (swagger): 📝 setup swagger documentation for verify/maintain endpoint

---
+++ get-responsible-by-id/swagger.js
@@ -0,0 +1,16 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API para Listar Responsable",
+      version: "1.0.0",
+      description: "API que permite obtener la información de un responsable por ID",
+    },
+  },
+  apis: ["./routes/responsibleRoutes.js"],  // Asegúrate de que este archivo sea el correcto
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+module.exports = swaggerSpec;
