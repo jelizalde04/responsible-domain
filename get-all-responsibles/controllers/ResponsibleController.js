76abc05e2c651757fc0a10f1f232e4a5ff335d97
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-04 19:55:38 -0500

    feat (controller): 👤 add getAllResponsibles function

---
+++ get-all-responsibles/controllers/ResponsibleController.js
@@ -0,0 +1,22 @@
+const ResponsibleService = require("../services/ResponsibleService");
+
+const getAllResponsibles = async (req, res, next) => {
+  try {
+    // Llamamos al servicio para obtener todos los responsables
+    const responsibles = await ResponsibleService.getAllResponsibles();
+
+    if (responsibles.length > 0) {
+      return res.status(200).json({
+        message: "Responsables encontrados.",
+        responsibles,  // Devolvemos los responsables encontrados
+      });
+    } else {
+      return res.status(404).json({ message: "No se encontraron responsables." });  // Si no hay responsables
+    }
+  } catch (error) {
+    console.error(error);
+    return next(error);  // Pasamos el error al middleware de manejo de errores
+  }
+};
+
+module.exports = { getAllResponsibles };
