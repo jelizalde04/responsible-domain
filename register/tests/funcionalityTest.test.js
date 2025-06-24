f549141884e208cf615807e65a90e86ac7f11e7b
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-06-24 12:24:32 -0500

    fix(tests): 🐛 correction the functional test

--- register/tests/funcionalityTest.test.js
+++ register/tests/funcionalityTest.test.js
@@ -1,10 +1,27 @@
 const fs = require('fs');
 const path = require('path');
+const sequelize = require('../config/db');
 
 describe('Register Microservice', () => {
-  test('Functionality test completed successfully', () => {
-    
-      const filePath = path.join(__dirname, '../config/db.js');
-  expect(fs.existsSync(filePath)).toBe(true);
+  test('db.js file exists', () => {
+    const filePath = path.join(__dirname, '../config/db.js');
+    expect(fs.existsSync(filePath)).toBe(true);
+  });
+
+  test('Database connection is established successfully', async () => {
+    expect.assertions(1);
+    try {
+      await sequelize.authenticate();
+      console.log('✅ Database connection test completed and approved successfully.');
+      expect(true).toBe(true); 
+    } catch (error) {
+      console.error('❌ Error connecting to the database:', error);
+      expect(error).toBeUndefined(); 
+    }
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+    console.log('✅ Database connection closed.');
   });
 });
