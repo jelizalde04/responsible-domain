1d703b6bc5dfe95478e7d2860972db1dcc7077db
Johana Elizalde <134255446+jelizalde04@users.noreply.github.com>
2025-07-15 14:12:34 -0500

    Initial commit

--- get-responsible-by-id/services/ResponsibleService.js
+++
@@ -1,9 +0,0 @@
-const Responsible = require("../models/Responsible");
-
-const getResponsibleById = async (id) => {
-  const responsible = await Responsible.findByPk(id);  
-
-  return responsible;  
-};
-
-module.exports = { getResponsibleById };
